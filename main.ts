20c5c068e4e83555fb32ecd020ba2a90bb49e82f
Jeff Caldwell <nemo.omen@gmail.com>
2022-12-14 13:49:07 -0600

    This lack of documentation kind of sucks

--- main.ts
+++ main.ts
@@ -1,49 +1,91 @@
 import './style.css';
-import { Engine, Actor, Color, CollisionType, Input } from 'https://esm.sh/excalibur@0.27.0';
+import { Engine, Actor, Color, CollisionType, Input, Physics, vec, Vector } from 'https://esm.sh/excalibur@0.27.0';
+
+// Physics.useRealisticPhysics();
+Physics.gravity = vec(0, 200);
 
 const game = new Engine({
+  height: document.body.clientHeight,
+  width: document.body.clientWidth,
   backgroundColor: Color.fromHex('#051126'),
 });
 
-game.start();
-
 const paddle = new Actor({
-  x: game.canvasWidth / 2,
-  y: game.canvasHeight / 2,
+  // x: game.halfDrawWidth,
+  // y: game.halfDrawHeight,
+  pos: vec(game.halfDrawWidth, game.halfDrawHeight),
   width: 30,
   height: 30,
   color: Color.Chartreuse
 });
 
-paddle.body.collisionType = CollisionType.Fixed;
+paddle.body.collisionType = CollisionType.active;
+paddle.body.useGravity = true;
 
-paddle.on('postupdate', () => {
-  if (game.input.keyboard.isHeld(Input.Keys.ArrowRight)) {
-    if (paddle.pos.x <= game.canvasWidth - (paddle.width / 2)) {
-      // paddle.pos.x += 20;
-      paddle.vel.x += 20;
-    }
-  } else if (game.input.keyboard.isHeld(Input.Keys.ArrowLeft)) {
-    if (paddle.pos.x >= paddle.width / 2) {
-      // paddle.pos.x -= 20;
-      paddle.vel.x -= 20;
-    }
-  } else {
-    if (paddle.vel.x <= 0) {
-      paddle.vel.x += 20;
-    }
-
-    if (paddle.vel.x >= 0) {
-      paddle.vel.x -= 20;
-    }
-  }
-
-  if (game.input.keyboard.wasReleased(Input.Keys.ArrowLeft)) {
-    if (paddle.pos.x <= paddle.width / 2) {
-      // paddle.pos.x += 20;
-    }
-  }
+const floor = new Actor({
+  x: game.halfDrawWidth,
+  y: game.drawHeight,
+  width: game.drawWidth,
+  height: 40,
+  color: Color.Gray,
+  collisionType: CollisionType.fixed,
 });
 
+const wall1 = new Actor({
+  x: 0,
+  y: game.halfDrawHeight,
+  width: 40,
+  height: game.drawHeight,
+  color: Color.Gray,
+  collisionType: CollisionType.fixed,
+});
 
-game.add(paddle);
+const wall2 = new Actor({
+  x: game.drawWidth,
+  y: game.halfDrawHeight,
+  width: 40,
+  height: game.drawHeight,
+  color: Color.Gray,
+  collisionType: CollisionType.fixed,
+});
+
+// paddle.body.collisionType = CollisionType.Fixed;
+
+paddle.on('postupdate', () => {
+  //   if (game.input.keyboard.isHeld(Input.Keys.ArrowRight)) {
+  //     if (paddle.pos.x <= game.drawWidth - paddle.width) {
+  //       // paddle.pos.x += 20;
+  //       paddle.vel.x += 20;
+  //     } else {
+  //       paddle.vel.x = 0;
+  //     }
+  //   } else if (game.input.keyboard.isHeld(Input.Keys.ArrowLeft)) {
+  //     if (paddle.pos.x >= paddle.width) {
+  //       // paddle.pos.x -= 20;
+  //       paddle.vel.x -= 20;
+  //     } else {
+  //       paddle.vel.x = 0;
+  //     }
+  //   } else {
+  //     if (paddle.vel.x <= 0) {
+  //       paddle.vel.x += 20;
+  //     }
+
+  //     if (paddle.vel.x >= 0) {
+  //       paddle.vel.x -= 20;
+  //     }
+  //   }
+
+  //   if (game.input.keyboard.wasReleased(Input.Keys.ArrowLeft)) {
+  //     if (paddle.pos.x <= paddle.width / 2) {
+  //       // paddle.pos.x += 20;
+  //     }
+  //   }
+});
+
+game.start().then(() => {
+  game.add(paddle);
+  game.add(floor);
+  game.add(wall1);
+  game.add(wall2);
+});
